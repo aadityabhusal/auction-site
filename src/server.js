fb56323afe853199acdf2e1c1f05ca1ffcc215c1
aadityabhusal <bhusal.001aditya@gmail.com>
2021-11-27 18:42:15 +0545

    refactor: updates mongodb code section in server.js file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,35 @@
+require("dotenv").config({ path: __dirname + "/.env" });
 const app = require("./app");
 const mongoose = require("mongoose");
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/AuctionSite", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose Connected to the Database");
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose Connection Disonnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
