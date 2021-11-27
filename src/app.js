7cd4cb6e2112d4510b3f06777fde589477a51c57
aadityabhusal <bhusal.001aditya@gmail.com>
2021-11-27 18:56:12 +0545

    feature: adds api routes for serving static files

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/", routes());
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 app.use((err, req, res, next) => {
   res.status(err.status).json({ error: err });
 });
